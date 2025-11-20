a83325cb33ed0c9b472eeb092a71595941a518e5
Rodrigo Rampazzo <rodrigo.rampazzo_citsoftware@prestador.globo>
2025-11-19 21:30:26 -0300

    feat: analytics queries

--- db/queries.py
+++ db/queries.py
@@ -15,5 +15,20 @@ def execute_query(query):
     else:
         st.error("Conexão ao banco de dados não pôde ser estabelecida.")
 
-def get_data_query():
-    return "SELECT * FROM usuario;"
+def get_ticket_medio_por_forma_pagamento_query():
+    return "SELECT formaPagto, AVG(valorTotal) AS ticket_medio \
+        FROM Pedido \
+        GROUP BY formaPagto;"
+
+def get_top_selling_products_query():
+    return "SELECT p.nome, SUM(c.quantidade) AS total_vendas \
+        FROM produto p \
+        JOIN item i ON p.idproduto = i.idproduto \
+        JOIN compra c ON i.iditem = c.iditem \
+        JOIN pedido pd ON c.idpedido = pd.idpedido \
+        WHERE pd.statuspagto = 'Pago' \
+        GROUP BY p.nome \
+        ORDER BY total_vendas DESC \
+        LIMIT 10;"
+
+            
